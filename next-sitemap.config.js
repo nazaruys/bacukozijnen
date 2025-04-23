3d435f48c523055ecd3926ca6ac1a751b1e5c5af
bacukozijnen <bacubouw@gmail.com>
2025-04-23 16:49:16 +0200

    Sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://www.bacukozijnen.nl',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
+  };
+
+export default config;
